59e976f73a284ca37f17460ba44d8ba88cd985d5
msmikeescom <mM02071980//>
2020-10-28 20:25:14 -0700

    Create simple layout for detail fragment.

--- app/src/main/java/com/mikeescom/doordashchallenge/view/DetailFragment.java
+++ app/src/main/java/com/mikeescom/doordashchallenge/view/DetailFragment.java
@@ -4,17 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.bumptech.glide.Glide;
 import com.mikeescom.doordashchallenge.R;
 import com.mikeescom.doordashchallenge.data.models.RestaurantDetail;
 import com.mikeescom.doordashchallenge.viewmodel.MainViewModel;
 
 public class DetailFragment extends Fragment {
     private MainViewModel viewModel;
+    private ProgressBar progressBar;
+
+    private TextView name;
+    private TextView description;
+    private TextView address;
+    private ImageView imageHeader;
 
     public DetailFragment() {
 
@@ -34,14 +43,37 @@ public class DetailFragment extends Fragment {
         viewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
         viewModel.init();
         viewModel.getRestaurantDetailResponseLiveData(id).observe(getViewLifecycleOwner(), response -> {
-            updateUI(view, response);
+            if (response != null) {
+                updateUI(response);
+            }
         });
+
+        initViews(view);
 
         return view;
     }
 
-    private void updateUI(View view, RestaurantDetail restaurantDetail) {
-        TextView textView = view.findViewById(R.id.name);
-        textView.setText(restaurantDetail.getName());
+    private void initViews(View view) {
+        progressBar = view.findViewById(R.id.progressBar);
+        imageHeader = view.findViewById(R.id.image_header);
+        name = view.findViewById(R.id.name);
+        description = view.findViewById(R.id.description);
+        address = view.findViewById(R.id.address);
+    }
+
+    private void updateUI(RestaurantDetail restaurantDetail) {
+        if (progressBar != null) {
+            progressBar.setVisibility(View.GONE);
+        }
+
+        Glide.with(getContext())
+                .load(restaurantDetail.getCover_img_url())
+                .centerCrop()
+                .placeholder(R.drawable.placeholder_item)
+                .into(imageHeader);
+
+        name.setText(restaurantDetail.getName());
+        description.setText(restaurantDetail.getDescription());
+        address.setText(restaurantDetail.getAddress().getPrintable_address());
     }
 }
--- app/src/main/java/com/mikeescom/doordashchallenge/view/ListFragment.java
+++ app/src/main/java/com/mikeescom/doordashchallenge/view/ListFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
@@ -13,11 +14,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.mikeescom.doordashchallenge.R;
+import com.mikeescom.doordashchallenge.data.models.Restaurant;
 import com.mikeescom.doordashchallenge.viewmodel.MainViewModel;
 
 public class ListFragment extends Fragment {
     private MainViewModel viewModel;
     private ListAdapter adapter;
+    private RecyclerView recyclerView;
+    private ProgressBar progressBar;
 
     public ListFragment() {
     }
@@ -38,11 +42,25 @@ public class ListFragment extends Fragment {
         viewModel.init();
         viewModel.getRestaurantsResponseLiveData(37.422740,-122.139956).observe(getViewLifecycleOwner(), response -> {
             if (response != null) {
-                adapter.setResults(response);
+                updateUI(response);
             }
         });
 
-        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        initViews(view);
+
+        return view;
+    }
+
+    private void initViews(View view) {
+        progressBar = view.findViewById(R.id.progressBar);
+        recyclerView = view.findViewById(R.id.recycler_view);
+    }
+
+    private void updateUI (Restaurant[] restaurants) {
+        if (progressBar != null) {
+            progressBar.setVisibility(View.GONE);
+        }
+        adapter.setResults(restaurants);
         recyclerView.addItemDecoration(new DividerItemDecoration(recyclerView.getContext()
                 , DividerItemDecoration.VERTICAL));
         recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
@@ -52,7 +70,5 @@ public class ListFragment extends Fragment {
             NavHostFragment.findNavController(getParentFragment()).navigate(R.id.action_listFragment_to_detailFragment, bundle);
         });
         recyclerView.setAdapter(adapter);
-
-        return view;
     }
 }
